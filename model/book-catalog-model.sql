cfabf069d6985d3a4688f1ed73c2dedf9748e805
Ruben <martinillo114@gmail.com>
2022-12-06 20:08:15 +0100

    BC-1 Added premiere field for book

--- model/book-catalog-model.sql
+++ model/book-catalog-model.sql
@@ -11,29 +11,18 @@ USE `book_catalog`;
 --
 
 DROP TABLE IF EXISTS `book`;
+
 CREATE TABLE `book` (
 book_id int NOT NULL AUTO_INCREMENT,
 isbn int NOT NULL,
 title varchar(100) NOT NULL,
 authors varchar(200) NOT NULL,
 publisher varchar(100) NOT NULL,
+premiere DATE NOT NULL,
 pages int NOT NULL,
 plot varchar(2000) NOT NULL,
 book_image varchar(1000),
-PRIMARY KEY(`id`,`isbn`)
-);
-
---
--- Table structure for table `user_book_collection` 
---
-
-DROP TABLE IF EXISTS `user_book_collection`;
-CREATE TABLE `user_book_collection`(
-user_id int,
-book_id int,
-PRIMARY KEY (`user_id`,`book_id`),
-CONSTRAINT `FK_user_id` FOREIGN KEY (`user_id`) REFERENCES user(`user_id`),
-CONSTRAINT `FK_user_id` FOREIGN KEY (`book_id`) REFERENCES book(`book_id`)
+PRIMARY KEY(`book_id`,`isbn`)
 );
 
 --
@@ -41,6 +30,7 @@ CONSTRAINT `FK_user_id` FOREIGN KEY (`book_id`) REFERENCES book(`book_id`)
 --
 
 DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user`(
 user_id int NOT NULL AUTO_INCREMENT,
 category varchar(20) DEFAULT 'flat' NOT NULL,
@@ -50,4 +40,17 @@ user_image varchar(1000),
 PRIMARY KEY(`user_id`)
 );
 
+--
+-- Table structure for table `user_book_collection` 
+--
+
+DROP TABLE IF EXISTS `user_book_collection`;
+
+CREATE TABLE `user_book_collection`(
+user_id int,
+book_id int,
+PRIMARY KEY (`user_id`,`book_id`),
+CONSTRAINT `FK_user_id` FOREIGN KEY (`user_id`) REFERENCES user(`user_id`), 
+CONSTRAINT `FK_book_id` FOREIGN KEY (`book_id`) REFERENCES book(`book_id`)
+);
 
